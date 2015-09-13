a03e3333292ab6d10880103c527f36edb9449f43
adiferd <adiferd@gmail.com>
2015-09-13 13:55:09 +0700

    Add stopwords list

--- Data.R
+++ Data.R
@@ -4,10 +4,8 @@ laporan[1] <-streamlaporan[3];
 mycorpus <- Corpus(VectorSource(laporan));
 mycorpus <- tm_map(mycorpus, stripWhitespace);
 mycorpus <- tm_map(mycorpus, tolower);
-mycorpus <- tm_map(mycorpus, stemDocument);
 mycorpus <- tm_map(mycorpus, PlainTextDocument);
+#mycorpus <- tm_map(mycorpus, stemDocument);
 #mycorpus <- tm_map(mycorpus, removeWords, stopwords("english"));
-wordcloud(mycorpus, scale=c(4,0.5), min.freq=3,max.words=300, random.order=FALSE, rot.per=0.35, use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
-
-
+wordcloud(mycorpus, scale=c(4,0.5),max.words=300, random.order=FALSE, rot.per=0.35, use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
 

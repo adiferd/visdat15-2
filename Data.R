5d7d54633d6b9c6fa2cbe8da11b3afa101975d87
adiferd <adiferd@gmail.com>
2015-09-13 14:55:08 +0700

    Ignoring Rhistory and Rapp.history

--- Data.R
+++ Data.R
@@ -1,11 +1,15 @@
 
-laporan[1] <-streamlaporan[3];
-
+laporan <-streamlaporan[3];
 mycorpus <- Corpus(VectorSource(laporan));
 mycorpus <- tm_map(mycorpus, stripWhitespace);
+mycorpus <- tm_map(mycorpus, removePunctuation) #menghapus tanda baca
+mycorpus <- tm_map(mycorpus, removeNumbers) #menghapus nomor
 mycorpus <- tm_map(mycorpus, tolower);
 mycorpus <- tm_map(mycorpus, PlainTextDocument);
-#mycorpus <- tm_map(mycorpus, stemDocument);
-#mycorpus <- tm_map(mycorpus, removeWords, stopwords("english"));
-wordcloud(mycorpus, scale=c(4,0.5),max.words=300, random.order=FALSE, rot.per=0.35, use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
+
+stopwordID <- "stopwords.txt";
+cStopwordID<-readLines(stopwordID);
+mycorpus <- tm_map(mycorpus, removeWords, c(stopwords("english"),cStopwordID);
+                   
+wordcloud(mycorpus, scale=c(3,0.2),max.words=300, random.order=FALSE, rot.per=0.35, use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
 

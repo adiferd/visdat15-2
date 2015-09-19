6aee1b792ec1ccab840aef53ac8ca44765307145
adiferd <adiferd@gmail.com>
2015-09-20 00:03:26 +0700

    Fixing Bubble Plot and add/remove data sources

--- BubblePlot.R
+++ BubblePlot.R
@@ -1,14 +1,22 @@
+rm(list = ls());
 setwd("/Users/adiferdian/Documents/Studi/Semester 3/Visualisasi Data/Tugas/visdat15-2");
-library(rjson);
-data <- read.csv("~/Documents/Studi/Semester 3/Visualisasi Data/Tugas/visdat15-2/Data/pegawaiNegeri.csv")
-symbols(data$Laki.Laki, 
-        data$Perempuan,
-        circles=data$Jumlah, 
-        inches = 0.4,
-        fg="white",
-        bg="gray",
-        xlab="Laki-laki",
-        ylab="Perempuan",
+pemegang <- read.csv("~/Documents/Studi/Semester 3/Visualisasi Data/Tugas/visdat15-2/Data/jumlahpemegangkartuaskesdikabupatenbojonegoro.csv", sep=";")
+pengguna <- read.csv("~/Documents/Studi/Semester 3/Visualisasi Data/Tugas/visdat15-2/Data/penggunaankartuaskesdikabupatenbojonegoro.csv", sep=";")
+library(plyr);
+pemegang$pemegang_kaartu <- NULL;
+pengguna$Penggunaan <- NULL;
+p1 <- ddply(pemegang, .(Tahun), summarise, jumPemegang=sum(Jumlah));
+p2 <- ddply(pengguna, .(Tahun), summarise, jumPengguna=sum(Jumlah));
+data <- merge(p1,p2,by="Tahun");
+symbols(data$Tahun, 
+        data$jumPemegang,
+        circles=data$jumPengguna, 
+        inches = 0.35,
+        fg=data$Tahun,
+        bg=data$Tahun,
+        xlab="Tahun",
+        ylab="Jumlah Pemegang",
+        xlim=c(20, xmax), ylim=c(ymin, ymax)
         main = "
-Jumlah Pegawai Negeri Sipil Menurut Provinsi 2014");
-text(data$Laki.Laki, data$Perempuan, data$Propinsi, cex=0.5);
+        Jumlah Penggunaan Askes Kabupaten Bojonegoro ");
+#text(data$Laki.Laki, data$Perempuan, data$Propinsi, cex=0.5);
